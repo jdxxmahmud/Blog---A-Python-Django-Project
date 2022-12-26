0a89d07905862e115b9651fb6240f6b2c011c41a
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-12-26 20:56:41 +0600

    added time and date in base.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+import datetime
 
 from django.http import Http404
 # Create your views here.
@@ -7,17 +8,21 @@ from django.http import Http404
 
 def post_list(request):
     posts = Post.objects.all()
-
+    timeNow = datetime.datetime.now()
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'date': datetime.date.today(),
+                   'time': timeNow.strftime("%I:%M %p")})
 
 
 def post_detail(request, id):
-
+    timeNow = datetime.datetime.now()
     post = get_object_or_404(Post,
                              id=id)
 
     return render(request,
                   'blog/post/detail.html',
-                  {"post": post})
+                  {"post": post,
+                   'date': datetime.date.today(),
+                   'time': timeNow.strftime("%I:%M %p")})
